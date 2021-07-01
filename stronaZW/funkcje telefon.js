9a24981716e1048582604ba144600a5a49af7397
MW-00 <86376368+MW-00@users.noreply.github.com>
2021-07-02 00:04:48 +0200

    zw na telefon

---
+++ stronaZW/funkcje telefon.js
@@ -0,0 +1,41 @@
+$(document).ready(function () {
+    
+// rozwijanie menu i pogrubienie
+
+$("#lista a").click(function(){
+    $(this).parent().siblings().children("ul").slideUp();
+    $(this).parent().siblings().children("ul").scrollTop(0);
+    $(this).parent().find("ul").slideToggle("slow");
+});
+
+$(".tytuly li a").click(function(){
+    $(".tytuly li a").removeClass("titleClicked");    
+    $(this).toggleClass("titleClicked");
+    $("#frame").attr("src", "podstrony/pusta.html");
+    $(".menu").slideUp();
+    $("footer").hide();
+    $(".main").show();
+});
+
+$(".rodzaj").click(function(){
+    if ($(this).hasClass("titleClicked")) {
+        $(this).removeClass("titleClicked");
+        $("#frame").attr("src", "podstrony/pusta.html");
+    }
+    else {
+        $(".rodzaj").removeClass("titleClicked");
+        $(this).addClass("titleClicked");
+    }
+});
+
+// monogram w pętli
+setInterval(function(){loop()},600);
+function loop(){
+    $("#monogram").fadeToggle(4000);
+}
+
+});
+
+  
+
+  
